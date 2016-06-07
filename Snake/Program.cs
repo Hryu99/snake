2c15d646e7b197b874fab477fb7011ac305b9b23
Yura.K <y.klushin@gmail.com>
2016-06-07 23:42:54 +0300

    class Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+
+            Draw(p1.x, p1.y, p1.sym);
+
+           
+
+
+            Console.ReadLine();
+        }
+
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
