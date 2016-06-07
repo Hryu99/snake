ab143fc1a8176aa6f37e0ed5131be4b6daec40fa
Yura.K <y.klushin@gmail.com>
2016-06-08 00:33:01 +0300

    class Figure added

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,13 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 10, '-');
-            line.Draw();
-            VerticalLine line2 = new VerticalLine(3, 10, 10, '*');
-            line2.Draw();
+          
 
            
 
